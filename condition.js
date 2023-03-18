4e11d7006d23d52d78be4a45888afe23a1f8d231
Kelvin Marcelo <marcelo.kelvinn@gmail.com>
2023-03-18 14:54:19 +0700

    condition.js

--- condition.js
+++ condition.js
@@ -50,3 +50,66 @@
 //     console.log ("Boleh Masuk")
 // }
 // }
+
+// var minuman = "kopi";
+// if (minuman.toLocaleLowerCase() == "kopi"){
+//     console.log("anda memilih kopi");
+//     console.log(minuman.toLocaleLowerCase());
+//     console.log(minuman.toLocaleUpperCase());
+// }
+// else{
+//     console.log("anda tidak memilih kopi");
+// }
+
+//Task 1
+/**
+ * Klasifikasi level untuk karakter dari sebuah game
+ * Jika power > 100, disebut "expert"
+ * Jika power > 70 - 100, disebut "pro"
+ * Jika power > 50 - 70, disebut "normal"
+ * Jika power > 30 - 50, disebut "basic"
+ * Jika power < 30, disebut "beginner"
+ */
+
+// var power = 25;
+// if(power > 100){
+//     console.log("Expert")} 
+//     else if (power > 70 && power <= 100){
+//         console.log ("Pro")}
+//     else if (power > 50 && power <=70){
+//         console.log ("Normal")}
+//     else if (power > 30 && power <= 50){
+//         console.log("Basic")}
+//     else (power < 30);{
+//         console.log("Beginner")
+//     }
+
+// var voucher = "FreeOngkir";
+// var pembelian = 50000;
+
+// switch (voucher){
+//     case "FreeOngkir":
+//         //Free Ongkir
+//         if(pembelian >= 50000){
+//             console.log("Total Pembelian Sebesar " + (pembelian));
+//             console.log("Potongan 10% sebesar = " + (pembelian*0.1));
+//             console.log("Biaya yang harus dibayar = " + (pembelian - (pembelian*0.1)));
+//         }
+//         else if ((pembelian >= 30000) && (pembelian < 50000)){
+//             console.log("Potongan 5%")
+//         }
+//         else
+//             console.log("Potongan 5%")
+//         break;
+//     case "Free 15%":
+//         //......
+//         if (pembelian >= 30000){
+//             console.log("Potongan 15%");
+//         }
+//         else{
+//             console.log("Potongan 0%")
+//         }
+//         break;
+//     default:
+//         console.log("Tidak ada voucher")
+// }
